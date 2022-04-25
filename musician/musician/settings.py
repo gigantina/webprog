db16ea34bbd9d808197d093707c02da71a82ebf6
gigantina <gigantina.ru@yandex.ru>
2022-04-25 20:13:51 +0300

    Base

--- musician/musician/settings.py
+++ musician/musician/settings.py
@@ -93,6 +93,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'mybase': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'web',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'PASSWORD': ''
     }
 }
 
